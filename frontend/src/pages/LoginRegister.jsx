dc9dd4cdd9e91ca827976478bb8802d233cde3de
Mohamed Said <misaid@ualberta.ca>
2024-07-15 03:41:23 -0600

    sync files

--- frontend/src/pages/LoginRegister.jsx
+++ frontend/src/pages/LoginRegister.jsx
@@ -10,18 +10,17 @@ const Login = () => {
       <Navbar />
       <div className="flex justify-center mt-10 ">
         <Tabs defaultValue="login" className="w-[400px]">
-          
           <TabsList className="">
             <TabsTrigger value="login">Login</TabsTrigger>
             <TabsTrigger value="register">Register</TabsTrigger>
           </TabsList>
           <div className="mt-4 px-3 pb-3 border border-solid rounded-xl">
-          <TabsContent value="login">
-            <LoginForm />
-          </TabsContent>
-          <TabsContent value="register">
-            <RegisterForm />
-          </TabsContent>
+            <TabsContent value="login">
+              <LoginForm />
+            </TabsContent>
+            <TabsContent value="register">
+              <RegisterForm />
+            </TabsContent>
           </div>
         </Tabs>
       </div>
