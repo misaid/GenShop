5d85cbef605e17c9cc1352eff2e0fc8f44de92b6
Mohamed Said <misaid@ualberta.ca>
2024-07-18 23:33:02 -0600

    ai tweaks

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,24 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import bcrypt from 'bcrypt';
-import rateLimit from 'express-rate-limit';
-import cookieParser from 'cookie-parser';
-import mongoose from 'mongoose';
-import jwt from 'jsonwebtoken';
-import OpenAI from 'openai';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bcrypt from "bcrypt";
+import rateLimit from "express-rate-limit";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import OpenAI from "openai";
 // models
-import User from './models/user.js';
-import Cart from './models/cart.js';
-import Comment from './models/comment.js';
-import Order from './models/order.js';
-import Product from './models/product.js';
+import User from "./models/user.js";
+import Cart from "./models/cart.js";
+import Comment from "./models/comment.js";
+import Order from "./models/order.js";
+import Product from "./models/product.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 const mongoDBURL = process.env.mongoDBURL;
 const secretKey = process.env.secretKey;
-const DOMAIN = process.env.domain || 'http://localhost:5001';
+const DOMAIN = process.env.domain || "http://localhost:5001";
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const openai = new OpenAI(OPENAI_API_KEY);
 
@@ -32,8 +32,6 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
 app.use(limiter);
 app.use(express.json());
-
-
 
 app.get("/", (request, response) => {
   console.log(request);
@@ -54,7 +52,11 @@ app.post("/register", async (request, response) => {
     const lower_username = username.toLowerCase();
     //create references to cart
     const new_cart = await Cart.create({});
-    const new_user = { username: lower_username, password: password_hash, cartid: new_cart._id }
+    const new_user = {
+      username: lower_username,
+      password: password_hash,
+      cartid: new_cart._id,
+    };
     const user = await User.create(new_user);
     //console.log(user);
     const token = jwt.sign({ userId: user._id }, secretKey);
@@ -64,8 +66,7 @@ app.post("/register", async (request, response) => {
       sameSite: "none",
     });
     return response.status(201).send("User created");
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
     return response.status(409).send("Username already exists");
   }
@@ -87,7 +88,7 @@ app.post("/login", async (request, response) => {
       return response.status(400).send("Invalid username or password");
     }
     const token = jwt.sign({ userId: user._id }, secretKey);
-    console.log(username, password)
+    console.log(username, password);
     response.cookie("token", token, {
       httpOnly: true,
       secure: true,
@@ -107,12 +108,15 @@ app.post("/logout", async (request, response) => {
 app.post("/generate", async (request, response) => {
   //console.log(request.body.prompt);
   try {
-    const promptInstructions = "You are an assistant that does nothing but respond to the prompt with a json with variables price (float), description, stock(int), and name for a potential product. With no additional description or context";
+    const promptInstructions =
+      "You are an assistant that does nothing but respond to the prompt with a json object with variables price (float), description, stock(int), and name for a potential product. With no additional description or context";
     const chatContent = promptInstructions + request.body.prompt;
     const completion = await openai.chat.completions.create({
       messages: [{ role: "system", content: chatContent }],
-      model: "gpt-3.5-turbo",
+      model: "gpt-4o-mini",
+      response_format: { type: "json_object" },
     });
+    console.log(completion.choices[0].message.content);
     //convert varibles to json
     const variables = JSON.parse(completion.choices[0].message.content);
     const name = variables.name;
@@ -121,27 +125,34 @@ app.post("/generate", async (request, response) => {
     const stock = variables.stock;
     console.log(variables);
     // create an image from variable name
-    const imageInstruction = "You are an assistant that does nothing but respond to the prompt with an image of the product based on the name the image must only contain a single item and be a product image with a white background this is the product name: ";
+    const imageInstruction =
+      "You are an assistant that does nothing but respond to the prompt with an image of the product based on the name the image must only contain a single item and be a product image with a white background this is the product name: ";
     const imagePrompt = imageInstruction + name;
     const imageCompletion = await openai.images.generate({
       model: "dall-e-3",
       prompt: imagePrompt,
       n: 1,
       size: "1024x1024",
-      response_format: 'b64_json',
+      response_format: "b64_json",
     });
     const image_url = imageCompletion.data[0].b64_json;
     //create product
-    const new_product = { name: name, price: price, image: image_url, countInStock: stock, description: description };
+    const new_product = {
+      name: name,
+      price: price,
+      image: image_url,
+      countInStock: stock,
+      description: description,
+    };
     const product = await Product.create(new_product);
-    return response.status(200).send({ "item": completion.choices[0].message.content, "image": image_url });
-  }
-  catch (error) {
+    return response
+      .status(200)
+      .send({ item: completion.choices[0].message.content, image: image_url });
+  } catch (error) {
     console.log(error);
     return response.status(400).send("Error in generating response");
-  };
+  }
 });
-
 
 mongoose
   .connect(mongoDBURL)
