e700910e15fdadfcc5d352c275da71a5eec179a3
Mohamed Said <misaid@ualberta.ca>
2024-07-20 03:01:36 -0600

    Price is now formatted using react-number-format

--- frontend/src/pages/components/Product.jsx
+++ frontend/src/pages/components/Product.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NumericFormat } from "react-number-format";
 
 const Product = ({ name, rating, stock, price, imageURL }) => {
   return (
@@ -17,7 +18,16 @@ const Product = ({ name, rating, stock, price, imageURL }) => {
           <div className="bg-green-100 h-5 w-24"> </div>
         </div>
         <div className="flex flex-col  bg-blue-100 h-38 w-full">
-          <h2 className="font-bold text-xl">${price}</h2>
+          <h2 className="font-bold text-xl">
+            <NumericFormat
+              value={price}
+              displayType={"text"}
+              thousandSeparator=","
+              prefix={"$"}
+              decimalScale={2}
+              fixedDecimalScale
+            />
+          </h2>
           <h3 className="font-semibold text-l mb-3">{name}</h3>
           <div className="flex items-center bg-green-200 border border-black rounded-xl h-8 w-20 hover:cursor-grab select-none">
             <h2 className="text-3xl mx-1">+</h2>
