9bec6e0bd1c864397261dd1f6c2459963350d7f7
Mohamed Said <misaid@ualberta.ca>
2024-07-23 14:25:34 -0600

    pagination work

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -2,13 +2,32 @@ import Navbar from "./components/Navbar";
 import Product from "./components/Product";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useLocation } from 'react-router-dom';
+//pagination
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+//
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const location = useLocation();
+  // const history = useHistory();
+  const searchParams = new URLSearchParams(location.search);
+  const page = searchParams.get("page") || 1;
+  const totalPages = 321;
+  const [currentPage, setCurrentPage] = useState(page);
   const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_APP_API_URL,
   });
   const fetchProducts = async () => {
     try {
+      console.log("fetching data for page", currentPage);
       const response = await axiosInstance.get("/products");
       setProducts(response.data);
     } catch (error) {
@@ -18,7 +37,7 @@ const Shop = () => {
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [currentPage]);
   return (
     <div>
       <Navbar />
@@ -37,7 +56,35 @@ const Shop = () => {
           ))}
         </div>
       </div>
-      Shop
+      <div></div>
+      <Pagination>
+        <PaginationContent>
+          {currentPage > 1 && (
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+          )}
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="/page=" >{currentPage}</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">{currentPage + 1}</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">321</PaginationLink>
+          </PaginationItem>
+          {currentPage == totalPages && (
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>)}
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 };
